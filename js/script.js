c83962fb6573cd5d22974065e1cc3aa730c7d317
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-01-29 10:40:52 +0200

    Ring pulsating

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const circle = document.querySelector('#circle');
+const ring = document.querySelector('.ringring');
 let scoreCount = 0;
 
 
@@ -6,10 +7,14 @@ setInterval(function() {
     const randomPosX = Math.floor((Math.random() * 100));
     const randomPosY = Math.floor((Math.random() * 100));
     const randomSize = Math.floor(Math.random() * 75) + 10;
-    circle.style.left = randomPosX + 'px';
-    circle.style.top = randomPosY + 'px';
-    circle.style.width = randomSize + 'px';
-    circle.style.height = randomSize + 'px';
+    let posX = circle.style.left = randomPosX + 'px';
+    let posY = circle.style.top = randomPosY + 'px';
+    let sW = circle.style.width = randomSize + 'px';
+    let sH = circle.style.height = randomSize + 'px';
+    ring.style.left = posX;
+    ring.style.top = posY;
+    ring.style.width = sW;
+    ring.style.height = sH;
 }, 1000);
 
 circle.addEventListener("click", function() {
